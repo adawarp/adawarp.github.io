44a382637031e051b9c0d0650d8306e756ed493e
cdra0624 <tatsukiadaniya@tatsuki.local>
2016-11-17 17:05:05 +0900

    enable simple video chat link

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const PORT = 3337;
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/docs/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.use("/", express.static(path.join(__dirname, 'docs')));
-app.use("/assets", express.static(path.join(__dirname, 'docs/assets')));
+app.use("/", express.static(path.join(__dirname, '/')));
+app.use("/assets", express.static(path.join(__dirname, '/assets')));
 
 app.listen(PORT);
 console.log('listening on port ' + PORT);
